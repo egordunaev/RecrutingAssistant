b3ffd4122b8f9cba195529d05162b827a8d544e5
Egor <egodunaev@gmail.com>
2019-04-15 00:15:47 +0300

    Interface updates

--- RecrutingAssistant/SettingsWindow.xaml.cs
+++ RecrutingAssistant/SettingsWindow.xaml.cs
@@ -11,6 +11,11 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using RA.BusinessLayer;
+using RA.Dto;
+using RecrutingAssistantApp.Properties;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace RecrutingAssistantApp
 {
@@ -22,6 +27,69 @@ namespace RecrutingAssistantApp
         public SettingsWindow()
         {
             InitializeComponent();
+            LoadSettings();
+        }
+        private bool recstatus;
+        private bool applySettings = false;
+        private void LoadSettings()
+        {
+            if (ProcessFactory.GetSettingsProcess().GetSettings() != null)
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.ConnectionString = ProcessFactory.GetSettingsProcess().GetSettings();
+                this.tbServer.Text = builder.DataSource;
+                this.tbDataBase.Text = builder.InitialCatalog;
+                if (builder.IntegratedSecurity & !recstatus)
+                {
+                    this.cbAuth.IsChecked = true;
+                }
+                else
+                {
+                    this.tbPassword.Text = builder.Password;
+                    this.tbUser.Text = builder.UserID;
+                }
+            }
+            else
+            {
+                this.tbServer.Text = "127.0.0.1";
+                this.tbDataBase.Text = "RecrutingDB";
+                this.cbAuth.IsChecked = true;
+            }
+        }
+
+        private void cbAuth_Checked(object sender, RoutedEventArgs e)
+        {
+            this.tbUser.Text = "";
+            this.tbPassword.Text = "";
+            this.tbUser.IsEnabled = false;
+            this.tbPassword.IsEnabled = false;
+        }
+
+        private void cbAuth_Unchecked(object sender, RoutedEventArgs e)
+        {
+            this.tbUser.IsEnabled = true;
+            this.tbPassword.IsEnabled = true;
+            this.recstatus = true;
+            LoadSettings();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (ProcessFactory.GetSettingsProcess().SetSettings(this.tbServer.Text, this.tbDataBase.Text, this.tbUser.Text, this.tbPassword.Text))
+            {
+                this.applySettings = true;
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Не удалось записать настройки! Что - то пошло не так");
+                return;
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
