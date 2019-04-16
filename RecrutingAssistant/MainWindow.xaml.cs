b8a9b577119cadcd99c1c89b021241453e301377
Egor <egodunaev@gmail.com>
2019-04-16 20:37:51 +0300

    Interface update
    Placeholders for working with tables

--- RecrutingAssistant/MainWindow.xaml.cs
+++ RecrutingAssistant/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RA.BusinessLayer;
+using RA.Dto;
+using RecrutingAssistantApp;
 
 namespace RecrutingAssistant
 {
@@ -20,29 +23,322 @@ namespace RecrutingAssistant
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string status; // Статус таблицы с которой работаем
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        // 
+        // ADD FUNCTIONS
+        //
         private void btnAdd_Click(object sender, RoutedEventArgs e)
         {
+            switch (status)
+            {
+                case "TypeOfWork":
+                    this.btnAddWork_Click(sender, e);
+                    break;
+                case "JobSeeker":
+                    this.btnAddSeeker_Click(sender, e);
+                    break;
+                case "Employer":
+                    this.btnAddEmployer_Click(sender, e);
+                    break;
+                case "Position":
+                    this.btnAddPosition_Click(sender, e);
+                    break;
+                case "Deal":
+                    this.btnAddDeal_Click(sender, e);
+                    break;
+                default:
+                    MessageBox.Show("Необходимо выбрать таблицу, в которую добавляется элемент!");
+                    return;
+            }
+        }
+        private void btnAddWork_Click(object sender, RoutedEventArgs e)
+        {
 
         }
+        private void btnAddSeeker_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+        private void btnAddEmployer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnAddPosition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnAddDeal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // 
+        // REFRESH FUNCTIONS
+        //
         private void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
+            switch (status)
+            {
+                case "TypeOfWork":
+                    this.btnRefreshWork_Click(sender, e);
+                    break;
+                case "JobSeeker":
+                    this.btnRefreshSeeker_Click(sender, e);
+                    break;
+                case "Employer":
+                    this.btnRefreshEmployer_Click(sender, e);
+                    break;
+                case "Position":
+                    this.btnRefreshPosition_Click(sender, e);
+                    break;
+                case "Deal":
+                    this.btnRefreshDeal_Click(sender, e);
+                    break;
+                default:
+                    MessageBox.Show("Необходимо выбрать таблицу, в которой обновляется элемент!");
+                    return;
+            }
+        }
+        private void btnRefreshWork_Click(object sender, RoutedEventArgs e)
+        {
 
         }
+        private void btnRefreshSeeker_Click(object sender, RoutedEventArgs e)
+        {
 
+        }
+        private void btnRefreshEmployer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnRefreshPosition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnRefreshDeal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // 
+        // DELETE FUNCTIONS
+        //
         private void btnDelete_Click(object sender, RoutedEventArgs e)
         {
-
+            switch (status)
+            {
+                case "TypeOfWork":
+                    this.btnDeleteWork_Click(sender, e);
+                    break;
+                case "JobSeeker":
+                    this.btnDeleteSeeker_Click(sender, e);
+                    break;
+                case "Employer":
+                    this.btnDeleteEmployer_Click(sender, e);
+                    break;
+                case "Position":
+                    this.btnDeletePosition_Click(sender, e);
+                    break;
+                case "Deal":
+                    this.btnDeleteDeal_Click(sender, e);
+                    break;
+                default:
+                    MessageBox.Show("Необходимо выбрать таблицу, в которой удаляется элемент!");
+                    return;
+            }
         }
-
-        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        private void btnDeleteWork_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+        private void btnDeleteSeeker_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnDeleteEmployer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnDeletePosition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnDeleteDeal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        // 
+        // EDIT FUNCTIONS
+        //
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            switch (status)
+            {
+                case "TypeOfWork":
+                    this.btnEditWork_Click(sender, e);
+                    break;
+                case "JobSeeker":
+                    this.btnEditSeeker_Click(sender, e);
+                    break;
+                case "Employer":
+                    this.btnEditEmployer_Click(sender, e);
+                    break;
+                case "Position":
+                    this.btnEditPosition_Click(sender, e);
+                    break;
+                case "Deal":
+                    this.btnEditDeal_Click(sender, e);
+                    break;
+                default:
+                    MessageBox.Show("Необходимо выбрать таблицу, в которой изменяется элемент!");
+                    return;
+            }
+        }
+        private void btnEditWork_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnEditSeeker_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnEditEmployer_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnEditPosition_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnEditDeal_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //
+        //   STATUS
+        //
+        private void btnTypeOfWork_Click(object sender, RoutedEventArgs e)
+        {
+            switch(status)
+            {
+                case "JobSeeker":this.dgJobSeeker.Visibility = Visibility.Hidden;
+                    break;
+                case "Employer":
+                    this.dgEmployer.Visibility = Visibility.Hidden;
+                    break;
+                case "Position":
+                    this.dgPosition.Visibility = Visibility.Hidden;
+                    break;
+                case "Deal":
+                    this.dgDeal.Visibility = Visibility.Hidden;
+                    break;
+            }
+            this.dgTypeOfWork.Visibility = Visibility.Visible;
+            status = "TypeOfWork";
+            this.btnRefresh_Click(sender, e);
+            this.statusLabel.Content = "Работа с таблицей: Виды работ";
+        }
+
+        private void btnJobSeeker_Click(object sender, RoutedEventArgs e)
+        {
+            switch (status)
+            {
+                case "TypeOfWork":
+                    this.dgTypeOfWork.Visibility = Visibility.Hidden;
+                    break;
+                case "Employer":
+                    this.dgEmployer.Visibility = Visibility.Hidden;
+                    break;
+                case "Position":
+                    this.dgPosition.Visibility = Visibility.Hidden;
+                    break;
+                case "Deal":
+                    this.dgDeal.Visibility = Visibility.Hidden;
+                    break;
+            }
+            this.dgJobSeeker.Visibility = Visibility.Visible;
+            status = "JobSeeker";
+            this.btnRefresh_Click(sender, e);
+            this.statusLabel.Content = "Работа с таблицей: Соискатели";
+        }
+
+        private void btnEmployer_Click(object sender, RoutedEventArgs e)
+        {
+            switch (status)
+            {
+                case "JobSeeker":
+                    this.dgJobSeeker.Visibility = Visibility.Hidden;
+                    break;
+                case "TypeOfWork":
+                    this.dgTypeOfWork.Visibility = Visibility.Hidden;
+                    break;
+                case "Position":
+                    this.dgPosition.Visibility = Visibility.Hidden;
+                    break;
+                case "Deal":
+                    this.dgDeal.Visibility = Visibility.Hidden;
+                    break;
+            }
+            this.dgEmployer.Visibility = Visibility.Visible;
+            status = "Employer";
+            this.btnRefresh_Click(sender, e);
+            this.statusLabel.Content = "Работа с таблицей: Работодатели";
+        }
+
+        private void btnPosition_Click(object sender, RoutedEventArgs e)
+        {
+            switch (status)
+            {
+                case "JobSeeker":
+                    this.dgJobSeeker.Visibility = Visibility.Hidden;
+                    break;
+                case "Employer":
+                    this.dgEmployer.Visibility = Visibility.Hidden;
+                    break;
+                case "TypeOfWork":
+                    this.dgTypeOfWork.Visibility = Visibility.Hidden;
+                    break;
+                case "Deal":
+                    this.dgDeal.Visibility = Visibility.Hidden;
+                    break;
+            }
+            this.dgPosition.Visibility = Visibility.Visible;
+            status = "Position";
+            this.btnRefresh_Click(sender, e);
+            this.statusLabel.Content = "Работа с таблицей: Вакансии";
+        }
+
+        private void btnDeal_Click(object sender, RoutedEventArgs e)
+        {
+            switch (status)
+            {
+                case "JobSeeker":
+                    this.dgJobSeeker.Visibility = Visibility.Hidden;
+                    break;
+                case "Employer":
+                    this.dgEmployer.Visibility = Visibility.Hidden;
+                    break;
+                case "Position":
+                    this.dgPosition.Visibility = Visibility.Hidden;
+                    break;
+                case "TypeOfWork":
+                    this.dgTypeOfWork.Visibility = Visibility.Hidden;
+                    break;
+            }
+            this.dgDeal.Visibility = Visibility.Visible;
+            status = "Deal";
+            this.btnRefresh_Click(sender, e);
+            this.statusLabel.Content = "Работа с таблицей: Сделки";
+        }
+        //
+        //  SETTINGS
+        //
+        private void btnDatabase_Click(object sender, RoutedEventArgs e)
+        {
+            SettingsWindow window = new SettingsWindow();
+            window.ShowDialog();
         }
     }
 }
